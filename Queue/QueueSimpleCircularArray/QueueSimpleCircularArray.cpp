eb7d4740fe91c86ee3f1eba2be1b3a7cc60b8b8b
ganesh tiwari <gat786@outlook.com>
2020-12-24 21:29:00 +0530

    Queue with Simple Circular Array implementation done without memory leaks

--- Queue/QueueSimpleCircularArray/QueueSimpleCircularArray.cpp
+++ Queue/QueueSimpleCircularArray/QueueSimpleCircularArray.cpp
@@ -5,6 +5,30 @@
 #include <iostream>
 #include "QueueSimpleCircularArray.hpp"
 
+/*
+ * QueueSimpleCircularArray queueCircularArray{};
+    auto queue = queueCircularArray.CreateQueue();
+    queueCircularArray.enqueueItem(queue,1);
+    queueCircularArray.enqueueItem(queue,2);
+    queueCircularArray.enqueueItem(queue,3);
+    queueCircularArray.enqueueItem(queue,4);
+    queueCircularArray.printQueue(queue);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    queueCircularArray.enqueueItem(queue,5);
+    queueCircularArray.enqueueItem(queue,6);
+    queueCircularArray.enqueueItem(queue,7);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    queueCircularArray.enqueueItem(queue,8);
+    queueCircularArray.enqueueItem(queue,9);
+    queueCircularArray.enqueueItem(queue,10);
+    queueCircularArray.enqueueItem(queue,11);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    queueCircularArray.enqueueItem(queue,12);
+    queueCircularArray.printQueue(queue);
+    queueCircularArray.deleteQueue(queue);
+ */
+
 struct QueueSimpleCircularArray::Queue * QueueSimpleCircularArray::CreateQueue() {
     auto queue = new Queue{};
     return queue;
@@ -64,10 +88,10 @@ void QueueSimpleCircularArray::printQueue(struct Queue *queue) {
     } else {
         if (queue->front != queue->rear) {
             int index = queue->front;
-            while (index % queue->capacity != queue->rear){
+            do {
                 std::cout << queue->array[index] << " | ";
-                ++index;
-            };
+                index = (++index) % queue->capacity;
+            } while (index % queue->capacity != queue->rear + 1);
         }else{
             std::cout << queue->array[queue->front] << " | ";
         }
--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 
 int main() {
     QueueSimpleCircularArray queueCircularArray{};
-    auto queue = queueCircularArray.CreateQueue(8);
+    auto queue = queueCircularArray.CreateQueue();
     queueCircularArray.enqueueItem(queue,1);
     queueCircularArray.enqueueItem(queue,2);
     queueCircularArray.enqueueItem(queue,3);
     queueCircularArray.enqueueItem(queue,4);
     queueCircularArray.printQueue(queue);
-    queueCircularArray.dequeueItem(queue);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
     queueCircularArray.enqueueItem(queue,5);
     queueCircularArray.enqueueItem(queue,6);
     queueCircularArray.enqueueItem(queue,7);
-    queueCircularArray.dequeueItem(queue);
-    queueCircularArray.dequeueItem(queue);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
     queueCircularArray.enqueueItem(queue,8);
     queueCircularArray.enqueueItem(queue,9);
     queueCircularArray.enqueueItem(queue,10);
+    queueCircularArray.enqueueItem(queue,11);
+    std::cout << "dequeuing item " << queueCircularArray.dequeueItem(queue) << std::endl;
+    queueCircularArray.enqueueItem(queue,12);
     queueCircularArray.printQueue(queue);
+    queueCircularArray.deleteQueue(queue);
 }
